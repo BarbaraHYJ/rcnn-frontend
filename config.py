eb73e1a26a49fe27e68b93a5461d851ea430889e
BarbaraBYJ <976397854@qq.com>
2018-05-20 16:12:44 +0800

    fix

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-UPLOAD_PATH = './files'
+UPLOAD_PATH = './files/demo'
 RESULT_PATH = './results'
 ALLOWED_EXTENSIONS = set(['jpg'])
 
-#DEMO_PATH = '/home/red/py-faster-rcnn/py-faster-rcnn/tools'
-DEMO_PATH = '/home/huangyijie/faster-rcnn/py-faster-rcnn/tools'
+DEMO_PATH = '/home/red/py-faster-rcnn/py-faster-rcnn/tools'
+#DEMO_PATH = '/home/huangyijie/faster-rcnn/py-faster-rcnn/tools'
